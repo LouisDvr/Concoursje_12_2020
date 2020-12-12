a7fb33dcaae7d0fe75f47d9c16195101589689d9
Louis <louis.develter@orange.fr>
2020-12-12 16:55:44 +0100

    Completed navigation bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,21 +11,20 @@ class NavBar extends Component {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt={"logo"} width="30" height="24" className="d-inline-block align-top"/>
-                            WireShield
+                    <a className="navbar-brand d-flex" href="#">
+                        <img src={logo} alt={"logo"} width="60" height="68" className="d-inline-block align-top"/>
+                        <p className="my-auto mx-3">WireShield</p>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
                             aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"/>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse ps-2" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            <a className="nav-link" href="#">Features</a>
-                            <a className="nav-link" href="#">Pricing</a>
-                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                            <a className="nav-link mx-2" href="#">Notre histoire</a>
+                            <a className="nav-link mx-2" href="#">Nos services</a>
+                            <a className="nav-link mx-2" href="#">Contact</a>
                         </div>
                     </div>
                 </div>
